4ea5965ff49c7c8960f79672f9ce878949128b9c
PrizZzt <npuzZzpak@yandex.ru>
2017-03-04 15:48:59 +0400

    Added pie diagram

--- BlackAccounting/ChartForm.cs
+++ BlackAccounting/ChartForm.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -11,7 +12,7 @@ namespace BlackAccounting
 	{
 		private Series _series;
 		private AccountingData _data;
-		private IList<Tuple<string, SeriesChartType, Action<Record>>> _chartParameters;
+		private IList<ChartParameters> _chartParameters;
 
 		public ChartForm(AccountingData accountingData)
 		{
@@ -19,27 +20,63 @@ namespace BlackAccounting
 
 			_series = chrtMain.Series.Add("");
 			_data = accountingData;
-			_chartParameters = new List<Tuple<string, SeriesChartType, Action<Record>>>();
+			_chartParameters = new List<ChartParameters>();
 
-			_chartParameters.Add(new Tuple<string, SeriesChartType, Action<Record>>("Потрачено за день в среднем", SeriesChartType.Line, r => { _series.Points.AddXY(r.Date, r.AvgForDay); }));
-			_chartParameters.Add(new Tuple<string, SeriesChartType, Action<Record>>("Остаток", SeriesChartType.Line, r => { _series.Points.AddXY(r.Date, r.AfterOperation); }));
-			_chartParameters.Add(new Tuple<string, SeriesChartType, Action<Record>>("Потрачено за день", SeriesChartType.Column, r => { _series.Points.AddXY(r.Date, r.ThisDayValue); }));
-			_chartParameters.Add(new Tuple<string, SeriesChartType, Action<Record>>("Потрачено за месяц", SeriesChartType.Column, r => { if (r.ThisMonthValue != null) _series.Points.AddXY(r.Date.ToString("MMMM yy"), r.ThisMonthValue); }));
+			_chartParameters.Add(new ChartParameters(
+				"Потрачено за день в среднем",
+				SeriesChartType.Line,
+				r => { _series.Points.AddXY(r.Date, r.AvgForDay); }
+				));
+			_chartParameters.Add(new ChartParameters(
+				"Остаток",
+				SeriesChartType.Line,
+				r => { _series.Points.AddXY(r.Date, r.AfterOperation); }
+				));
+			_chartParameters.Add(new ChartParameters(
+				"Потрачено за день",
+				SeriesChartType.Column,
+				r => { _series.Points.AddXY(r.Date, r.ThisDayValue); }
+				));
+			_chartParameters.Add(new ChartParameters(
+				"Потрачено за месяц",
+				SeriesChartType.Column,
+				r => { if (r.ThisMonthValue != null) _series.Points.AddXY(r.Date.ToString("MMMM yy"), r.ThisMonthValue); }
+				));
+			_chartParameters.Add(new ChartParameters(
+				"Структура расходов",
+				(data, series) =>
+				{
+					_series.ChartType = SeriesChartType.Pie;
+					foreach (var r in data.GroupBy(r => r.TypeID).Select(gr => new { Key = _data.Types[gr.Key].Description, Value = gr.Where(r => r.Value < 0).Sum(r => -r.Value) }).OrderBy(r=>r.Value))
+					{
+						if (r.Value > 0)
+							series.Points.AddXY(r.Key, r.Value);
+					}
+				}
+				));
 
 			cmbChartTypeSelect.DataSource = _chartParameters;
 			cmbChartTypeSelect.ValueMember = "";
-			cmbChartTypeSelect.DisplayMember = "Item1";
+			cmbChartTypeSelect.DisplayMember = "Description";
 		}
 
 		private void cmbChartTypeSelect_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			var selected = (Tuple<string, SeriesChartType, Action<Record>>)cmbChartTypeSelect.SelectedValue;
+			var selected = (ChartParameters)cmbChartTypeSelect.SelectedValue;
 
 			_series.Points.Clear();
-			_series.ChartType = selected.Item2;
-			foreach (Record r in _data.Records)
+			if (selected.FullFillAction != null)
+				selected.FullFillAction(_data.Records, _series);
+			else
 			{
-				selected.Item3(r);
+				if (selected.FillAction != null)
+				{
+					_series.ChartType = selected.ChartType;
+					foreach (Record r in _data.Records)
+					{
+						selected.FillAction(r);
+					}
+				}
 			}
 		}
 	}
---
+++ BlackAccounting/ChartParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace BlackAccounting
+{
+	/// <summary>
+	/// Параметры для построения графика
+	/// </summary>
+	class ChartParameters
+	{
+		/// <summary>
+		/// Описание графика
+		/// </summary>
+		public string Description { get; private set; }
+		/// <summary>
+		/// 
+		/// </summary>
+		public SeriesChartType ChartType { get; private set; }
+		/// <summary>
+		/// Действие для добавления записи на график
+		/// </summary>
+		public Action<Record> FillAction { get; private set; }
+		/// <summary>
+		/// Действие для полного управления процессом построения графика
+		/// </summary>
+		public Action<IEnumerable<Record>, Series> FullFillAction { get; private set; }
+
+		public ChartParameters(string description, SeriesChartType chartType, Action<Record> fillAction)
+		{
+			Description = description;
+			ChartType = chartType;
+			FillAction = fillAction;
+			FullFillAction = null;
+		}
+		public ChartParameters(string description, Action<IEnumerable<Record>, Series> fullFillAction)
+		{
+			Description = description;
+			ChartType = SeriesChartType.FastPoint;
+			FillAction = null;
+			FullFillAction = fullFillAction;
+		}
+	}
+}
